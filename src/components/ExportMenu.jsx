414d6b8c4c73bfb67f9b4a0e147d7cc6f26a5331
rohan <rohanwarrier01@gmail.com>
2024-05-17 17:59:54 +1000

    added yaml export option

--- src/components/ExportMenu.jsx
+++ src/components/ExportMenu.jsx
@@ -1,16 +1,26 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, Typography } from '@mui/material';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, Typography } from '@mui/material';
 import React, { useRef } from 'react';
 import { ErasMedium } from './Eras';
+import yaml from 'js-yaml';
 
 const ExportMenu = ({ open, setOpen, data }) => {
     const downloadRef = useRef();
 
-    const handleExport = () => {
+    const handleExportJson = () => {
         const json = JSON.stringify(data, null, 2);
         const blob = new Blob([json], { type: 'application/json' });
         const url = URL.createObjectURL(blob);
         downloadRef.current.href = url;
         downloadRef.current.download = 'data.json';
+        downloadRef.current.click();
+    }
+
+    const handleExportYaml = () => {
+        const yamlData = yaml.dump(data);
+        const blob = new Blob([yamlData], { type: 'appliction/x-yaml' });
+        const url = URL.createObjectURL(blob);
+        downloadRef.current.href = url;
+        downloadRef.current.download = 'data.yaml';
         downloadRef.current.click();
     }
 
@@ -22,11 +32,14 @@ const ExportMenu = ({ open, setOpen, data }) => {
             <Divider />
             <DialogContent>
                 <Typography>
-                    Export the graph data in a JSON format.
+                    Export the results of the algorithm.
                 </Typography>
             </DialogContent>
             <DialogActions>
-                <Button onClick={handleExport} variant='contained' color='info' sx={{ mt: 2 }}>Export</Button>
+                <Box sx={{ display: 'flex', gap: 2 }}>
+                    <Button onClick={handleExportJson} variant='contained' color='info'>Export to JSON</Button>
+                    <Button onClick={handleExportYaml} variant='contained' color='info'>Export to YAML</Button>
+                </Box>
                 <a ref={downloadRef} style={{ display: 'none' }} />
             </DialogActions>
         </Dialog>
