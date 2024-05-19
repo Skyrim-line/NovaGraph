97a6b01cec0508965bbff57cda04098318b2ac99
rohan <rohanwarrier01@gmail.com>
2024-05-19 16:19:44 +1000

    added screenshots for the image carousel

--- src/components/HelperCarousel.jsx
+++ src/components/HelperCarousel.jsx
@@ -1,16 +1,43 @@
 import { Paper, Box, Dialog, DialogTitle, Divider, DialogContent, Typography, useTheme } from "@mui/material";
-import { ErasMedium } from "./Eras";
+import { ErasBold, ErasMedium } from "./Eras";
 import { useState } from "react";
 import Carousel from "react-material-ui-carousel";
 
 const images = [
   {
-    label: "Label1",
-    img: "./logo.png"
+    label: "1. Graph Layout",
+    img: "./default-graph.png",
+    text: "A default graph consisting of European cities will be displayed initially."
   },
   {
-    label: "Label2",
-    img: "./logo.png"
+    label: "2. Algorithms",
+    img: "./graph-algorithms.png",
+    text: "The sidebar on the right contains various graph algorithms that can be applied to the current graph."
+  },
+  {
+    label: "3. Algorithm Results",
+    img: "./algorithm-output.png",
+    text: "After running an algorithm, the graph will be updated to show the results. Further details can be found underneath the graph."
+  },
+  {
+    label: "4. Moving Nodes",
+    img: "./simulation.png",
+    text: "Buttons on the left can control the movement of nodes in the graph including fitting all the nodes in the view."
+  },
+  {
+    label: "5. Graph Options",
+    img: "./graph-options.png",
+    text: "Buttons on the right control the appearance of the graph which can be useful for displaying large graphs."
+  },
+  {
+    label: "6. Import Graph",
+    img: "./import.png",
+    text: "The Import button allows you to load a graph from a file or generate a random graph."
+  },
+  {
+    label: "7. Export Graph",
+    img: "./export.png",
+    text: "The Export button allows you to save the results of the current algorithm to a file."
   }
 ];
 
@@ -29,13 +56,13 @@ const HelperCarousel = ({ open, handleClose }) => {
       maxWidth='lg'
     >
       <DialogTitle>
-        <ErasMedium>Help</ErasMedium>
+        <ErasMedium>How to use Novagraph</ErasMedium>
       </DialogTitle>
       <Divider />
       <DialogContent>
-        <Paper height={50} sx={{ p: 1 }}>
-          <Typography>{images[activeStep].label}</Typography>
-        </Paper>
+        <Box sx={{ textAlign: 'center' }} mb={1}>
+          <ErasBold>{images[activeStep].label}</ErasBold>
+        </Box>
         <Carousel
           animation='slide'
           autoPlay
@@ -43,6 +70,7 @@ const HelperCarousel = ({ open, handleClose }) => {
           index={activeStep}
           onChange={(index, active) => handleStepChange(index)}
           cycleNavigation
+          navButtonsAlwaysVisible
           sx={{
             '.MuiPaper-root': {
               backgroundColor: 'transparent',
@@ -73,6 +101,11 @@ const HelperCarousel = ({ open, handleClose }) => {
             </Box>
           ))}
         </Carousel>
+        <Box sx={{ textAlign: 'center' }} p={2}>
+          <Typography variant='body1' sx={{ mt: 1 }}>
+            {images[activeStep].text}
+          </Typography>
+        </Box>
       </DialogContent>
     </Dialog>
   );
